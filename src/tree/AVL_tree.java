0013a6a498aadb6ec0e15795ba308045c1062f69
Shengbo Lou <loushengbo@hotmail.com>
2016-03-01 14:31:26 -0500

    delete

--- src/tree/AVL_tree.java
+++ src/tree/AVL_tree.java
@@ -1,7 +1,5 @@
 package tree;
 
-import java.awt.RenderingHints.Key;
-
 public class AVL_tree {
 	
 	static Node root;
@@ -76,6 +74,90 @@ public class AVL_tree {
 		}
 		
 		return node;
+	}
+	Node Successor(Node node){
+		Node tmp = node.right;
+		if(tmp == null)
+			return null;
+		else{
+			while(tmp.left != null){
+				tmp = tmp.left;
+			}
+			return tmp;
+		}
+	}
+	
+	Node Delete(Node node,int data){
+		//perform normal binary search tree deletion
+		if (node == null) {
+			return node;
+		}
+		else if(node.data < data)
+			node.right = Delete(node.right, data);
+		else if(node.data > data)
+			node.left = Delete(node.left, data);
+		//find the node to delete
+		else{
+			//if node only has one child
+			if(node.left == null || node.right == null){
+				
+				
+				//no child
+				if(node.left == null && node.right == null){
+					node = null;
+				}
+				else if(node.left == null){
+					node = node.right;
+				}
+				else{
+					node = node.left;
+				}
+			}
+			//node has two children
+			else{
+				Node successor = Successor(node);
+				node.data = successor.data;
+				node.right = Delete(node.right, successor.data);
+			}
+		}
+		
+		int balance = Balance_factor(node);
+		
+		//update current height of node
+		node.height = Math.max(height(node.left), height(node.right))+1;
+		
+		//left right case
+		if(balance > 1 && Balance_factor(node.left)<0){
+			node.left = RotateRight(node.left);
+			return RotateRight(node);
+		}
+		//left left case
+		if(balance >1 && Balance_factor(node.left)>=0){
+			return RotateRight(node);
+		}
+		
+		//right left case
+		if(balance < -1 && Balance_factor(node.right)>0){
+			node.right = RotateLeft(node.right);
+			return RotateRight(node);
+		}
+		
+		//right right case
+		if(balance < -1 && Balance_factor(node.right)<=0){
+			return RotateLeft(node);
+		}
+		
+		return node;
+	}
+	Node Search(Node node, int data){
+		if(node == null)
+			return null;
+		if (node.data == data) {
+			return node;
+		}
+		else if(node.data < data)
+			return Search(node.right, data);
+		else return Search(node.left, data);
 	}
 	
 	void PreOrder(Node node){
--- src/tree/Main.java
+++ src/tree/Main.java
@@ -5,14 +5,21 @@ public class Main {
 	public static void main(String[] args){
 		AVL_tree tree = new AVL_tree();
 
-		Node root = new Node(10);
+		Node root = new Node(9);
 		
-		root = tree.insert(root, 20);
-		root = tree.insert(root, 30);
-		root = tree.insert(root, 40);
-		root = tree.insert(root, 50);
-		root = tree.insert(root, 25);
+		root = tree.insert(root, 5);
+		root = tree.insert(root, 10);
+		root = tree.insert(root, 0);
+		root = tree.insert(root, 6);
+		root = tree.insert(root, 11);
+		root = tree.insert(root, -1);
+		root = tree.insert(root, 1);
+		root = tree.insert(root, 2);
+		
+		root = tree.Delete(root, 10);
 		
 		tree.PreOrder(root);
+		
+		System.out.println(tree.Search(root, 1111));
 	}
 }
